d376d3550e34617ddd9d8434148f51d9cb9ccc9d
Kera <kera.andrea@gmail.com>
2018-08-21 17:12:06 -0400

    inital functionality changes

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,1 +1,33 @@
-console.log('Hiii');
+const app = {}
+
+app.locations = function (def) {
+    $.ajax({
+        url: `https://restcountries.eu/rest/v2/name/${def}`,
+        method: 'GET',
+        dataType: 'json'
+    }).then((res) => {
+        console.log(res);
+    })
+}
+
+app.countryName = function() {
+    $('form').on('submit', function (e){
+        e.preventDefault();
+        let countrySelection = $('.vacationSearch').val();
+        app.locations(countrySelection);
+        console.log(countrySelection);
+    });
+    
+}
+
+app.init = function () {
+    app.countryName();
+}
+
+$(function() {
+    console.log( "ready!" );
+    app.init();
+});
+
+// We want to get the users vacation location 
+// We want to save it to countryName 
